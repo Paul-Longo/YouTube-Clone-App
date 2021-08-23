96da3886606aea8eaa373661429d95794b22a308
Paul-Longo <imanazarite88@gmail.com>
2021-08-23 11:30:40 -0400

    updated search bar. still in progress to become functional

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,18 +1,33 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            search:'base',
-            searchResults: [],
+        this.state = {
+            searhTerm: ""
         }
+    }    
+    
+    handleChange =(event) => {
+        this.setState({
+            searchTerm: event.target.value
+        });
     }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.handleSearch(this.state.searchTerm)
+    }
+
+
+
     render() { 
         return ( 
             <div>
-                <form className='Search' onSubmit={}>
-                    <input name='field' onChange={} />
+                <form onSubmit={this.handleSubmit} className='Search' >
+                    <input name='searchBar' type="text" placeholder="Search" onChange={(event) => this.handleChange(event)} />
+                    <button type="submit">Let's Go</button>
                 </form>
             </div>
          );
